5e367ef85df199a06319dbfa7be22caa451a91d3
PdgarHern <pdgaher@gmail.com>
2022-02-04 16:47:31 +0000

    1st test ok

--- cardiology_backend/test/test_helper.rb
+++ cardiology_backend/test/test_helper.rb
@@ -6,6 +6,24 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors, with: :threads)
 
+  # Returns true if a test user is logged in.
+  # def is_logged_in?
+  #   !session[:user_id].nil?
+  # end
+
+  # Logs in a test user.
+  # def log_in_as(user, options = {})
+  #   password    = options[:password]    || 'password'
+  #   remember_me = options[:remember_me] || '1'
+  #   if integration_test?
+  #     post login_path, session: { email:       user.email,
+  #                                 password:    password,
+  #                                 remember_me: remember_me }
+  #   else
+  #     session[:user_id] = user.id
+  #   end
+  # end
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 

9b864590a7414d9cc15c7c20f86da94b7de61823
PdgarHern <pdgaher@gmail.com>
2022-02-04 16:29:03 +0000

    devise-commit

---
+++ cardiology_backend/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  default_url_options :host => "localhost:3000"
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+             
+end
